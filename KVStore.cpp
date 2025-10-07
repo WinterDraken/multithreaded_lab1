2d041ba1fbc53fb59e83d63be9d7061ecab2265e
WinterDraken <flamewyrn15@gmail.com>
2025-10-06 20:40:25 -0500

    Decided to put snapshots in the kvstore instead of a seperate class, will be storing a series of snapshots in a vector. You will be able to add, remove from the snapshot vector as needed.

--- KVStore.cpp
+++ KVStore.cpp
@@ -1,5 +1,7 @@
 #include <unordered_map>
 #include <string>
+#include <memory>
+#include <vector>
 #include "FairShareMutex.hpp"
 
 template <typename Key, typename Value>
@@ -7,7 +9,9 @@ class KVStore{
     private:
         std::unordered_map<Key, Value> store;
         FairShareMutex rw_lock;
-    
+
+        std::vector<std::shared_ptr<const std::unordered_map<Key, Value>>> snapshot_vector;
+
     public:
         void put(const Key& key, const Value& value){
             rw_lock.lock();
@@ -32,6 +36,31 @@ class KVStore{
             rw_lock.shared_unlock();
             return false;
 
+        }
+
+        std::shared_ptr<const std::unordered_map<Key, Value>> snapshot(){
+            rw_lock.shared_lock();
+            auto snap = std::make_shared<const std::unordered_map <Key, Value>>(store);
+            rw_lock.shared_unlock();
+
+            snapshot_vector.push_back(snap);
+            return snap;
+        }
+
+        const std::vector<std::shared_ptr<const std::unordered_map<Key, Value>>>& get_snapshots() const {
+            return snapshot_vector;
+        }
+
+        void clear_snapshots() {
+            snapshot_vector.clear();
+        }
+        
+        std::shared_ptr<const std::unordered_map<Key, Value>> get_snapshot(size_t index) const {
+            if(index < snapshot_vector.size()) {
+                return snapshot_vector[index];
+            } else {
+                return nullptr; // no snapshot at this index
+            }
         }
 
 
